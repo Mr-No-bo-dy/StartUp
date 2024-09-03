eab11b4496fcfd17802db8a7c2aa91779ab5e35b
Mr-No-bo-dy <o.m.gutsalyuk@gmail.com>
2024-09-03 18:41:19 +0300

    added scripts: show/hide nav, smooth scroll, bg_parallax, regex, sign popup

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,13 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
     function navToggle() {
         hamburger.classList.toggle('hamburger_open');
         nav.classList.toggle('nav_mobile');
-    
+
         if (window.matchMedia('(max-width: 500px)').matches) {
             document.body.classList.toggle('over_hidden');
         } else {
             document.body.classList.remove('over_hidden');
         }
-    
+
         if (hamburger.classList.contains('hamburger_open')) {
             window.addEventListener('resize', bodyOverToggle);
         } else {
@@ -32,5 +32,138 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     }
+
+    // Show/Hide Nav menu
+    const header = document.querySelector('#header .header_line');
+    let lastScroll = 0;
+    document.addEventListener('scroll', function() {
+        let scroll = window.scrollY;
+        if (scroll > lastScroll) {
+            header.style.top = '-100%';
+        } else {
+            header.style.top = 0;
+        }
+        lastScroll = scroll;
+    });
+
+    // Smooth scroll to certain block on same page
+    document.querySelectorAll('.header_menu a').forEach(link => {
+        link.addEventListener('click', function(ev) {
+            ev.preventDefault();
+            const toBlock = document.querySelector(link.getAttribute('href'));
+            toBlock.scrollIntoView({behavior: 'smooth'});
+        });
+    });
+
+    // Parallax on backgrounds
+    const blocks = document.querySelectorAll('.bg_parallax');
+    blocks.forEach(block => {
+        function parallax(ev) {
+            let moveX = (((ev.pageX - window.scrollX) / window.innerWidth) - 0.5) * 70,
+                moveY = (((ev.pageY - window.scrollY) / window.innerHeight) - 0.5) * 20;
+            block.style.backgroundPosition = `${50 + moveX}% ${50 + moveY}%`;
+        }
+        block.addEventListener('mousemove', parallax);
+        if (!block) block.removeEventListener('mousemove', parallax);
+    });
+
+    // Phone check through RegEx
+    const tel = document.querySelector('#tel'),
+        signBtn = document.querySelector('#signBtn'),
+        phoneRegEx1 = /^(\+\d{1,3}[-\s]?)?(\(?\d{3}\)?[-\s]?)\d{3}[-\s]?\d{2}[-\s]?\d{2}$/g,
+        phoneRegEx2 = /^(\+\d{1,3}[-\s]?)?(\(?\d{3}\)?[-\s]?)\d{2}[-\s]?\d{3}[-\s]?\d{2}$/g,
+        phoneRegEx3 = /^(\+\d{1,3}[-\s]?)?(\(?\d{3}\)?[-\s]?)\d{2}[-\s]?\d{2}[-\s]?\d{3}$/g;
+    let errorTel = document.querySelector('#errorTel');
+    let signErrors = [];
+    signErrors['tel'] = true;
+    tel.oninput = function() {
+        let string = tel.value.replace(/[^+0-9\(\)\-\s]/g, '');
+        tel.value = string;
+        if (string.length >= 10) {
+            if ((string.includes('(') && !string.includes(')')) || (!string.includes('(') && string.includes(')'))) {
+                signErrors['tel'] = true;
+                signBtn.setAttribute('disabled', '');
+                errorTel.innerHTML = 'Неправильно введений номер';
+            } else if (phoneRegEx1.test(string) || phoneRegEx2.test(string) || phoneRegEx3.test(string)) {
+                signErrors['tel'] = false;
+                errorTel.innerHTML = '';
+            } else {
+                if (phoneRegEx1.test(string) || phoneRegEx2.test(string) || phoneRegEx3.test(string)) {
+                    signErrors['tel'] = false;
+                    errorTel.innerHTML = '';
+                } else {
+                    signErrors['tel'] = true;
+                    signBtn.setAttribute('disabled', '');
+                    errorTel.innerHTML = 'Неправильно введений номер';
+                }
+            }
+        } else {
+            signErrors['tel'] = true;
+            signBtn.setAttribute('disabled', '');
+        }
+        // console.log(signErrors['tel'], signErrors['email']);
+        if (!signErrors['tel'] && !signErrors['email']) {
+            signBtn.removeAttribute('disabled');
+        }
+    }
+
+    // Email check through RegEx
+    const emails = document.querySelectorAll('.email'),
+        emailRegEx = /^([a-z][a-z0-9._-]+[a-z])(@[a-z][a-z0-9_-]+)(\.[a-z]+)(\.[a-z]+)?$/gi;
+    signErrors['email'] = true;
+    emails.forEach(email => {
+        const errorEmail = email.nextElementSibling;
+        email.oninput = function() {
+            let string = email.value;
+            if (string.length >= 10) {
+                if (emailRegEx.test(string)) {
+                    signErrors['email'] = false;
+                    errorEmail.innerHTML = '';
+                } else {
+                    if (emailRegEx.test(string)) {
+                        signErrors['email'] = false;
+                        errorEmail.innerHTML = '';
+                    } else {
+                        signErrors['email'] = true;
+                        signBtn.setAttribute('disabled', '');
+                        errorEmail.innerHTML = 'Такої ел. адреси не існує';
+                    }
+                }
+            } else {
+                signErrors['email'] = true;
+                signBtn.setAttribute('disabled', '');
+            }
+            // console.log(signErrors['tel'], signErrors['email']);
+            if (!signErrors['tel'] && !signErrors['email']) {
+                signBtn.removeAttribute('disabled');
+            }
+        }
+    });
+
+    // Sign popup
+    const popup = document.querySelector('#sign_popup'),
+        closeBtn = popup.querySelector('#closeBtn');
+    function showPopup() {
+        popup.classList.remove('hidden');
+        document.body.classList.add('over_hidden');
+
+        // Close popup
+        function closePopup() {
+            popup.classList.add('hidden');
+            document.body.classList.remove('over_hidden');
+            signErrors['tel'] = false;
+            signErrors['email'] = false;
+        }
+        popup.addEventListener('click', function(ev) {
+            if (ev.target === popup) closePopup();
+        });
+        document.addEventListener("keydown", function (ev) {
+            if (ev.key === "Escape") closePopup();
+        });
+        closeBtn.onclick = closePopup;
+    }
+    document.querySelector('#getStarted').addEventListener('click', showPopup);
+
+
 
 });
