e63b788e61ec623b1ba3bbf29fdaba45937475a6
Mr-No-bo-dy <o.m.gutsalyuk@gmail.com>
2024-09-07 18:56:58 +0300

    scripts: unfold text, confirm form_submit, work w/localStorage; minor changes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -44,6 +44,13 @@ document.addEventListener('DOMContentLoaded', function () {
             header.style.top = 0;
         }
         lastScroll = scroll;
+    
+        // Change header's background-color
+        if (window.scrollY > header.offsetHeight) {
+            header.classList.add('header_bgc');
+        } else {
+            header.classList.remove('header_bgc');
+        }
     });
 
     // Smooth scroll to certain block on same page
@@ -65,6 +72,10 @@ document.addEventListener('DOMContentLoaded', function () {
     //         });
     //     });
     // });
+    document.querySelector('#getInTouchBtn').addEventListener('click', function (ev) {
+        ev.preventDefault();
+        document.querySelector('#contactForm').scrollIntoView({behavior: 'smooth'});
+    });
 
     // Parallax on backgrounds
     const blocks = document.querySelectorAll('.bg_parallax');
@@ -80,50 +91,54 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Phone check through RegEx
     const tel = document.querySelector('#tel'),
-        signBtn = document.querySelector('#signBtn'),
         phoneRegEx1 = /^(\+\d{1,3}[-\s]?)?(\(?\d{3}\)?[-\s]?)\d{3}[-\s]?\d{2}[-\s]?\d{2}$/g,
         phoneRegEx2 = /^(\+\d{1,3}[-\s]?)?(\(?\d{3}\)?[-\s]?)\d{2}[-\s]?\d{3}[-\s]?\d{2}$/g,
         phoneRegEx3 = /^(\+\d{1,3}[-\s]?)?(\(?\d{3}\)?[-\s]?)\d{2}[-\s]?\d{2}[-\s]?\d{3}$/g;
-    let errorTel = document.querySelector('#errorTel');
-    let signErrors = [];
-    signErrors['tel'] = true;
+    let errorTel = document.querySelector('#errorTel'),
+        disabledBtn = tel.closest('form').querySelector('.disabledBtn'),
+        signErrors = [];
+    signErrors['tel'] = false;
     tel.oninput = function () {
         let string = tel.value.replace(/[^+0-9\(\)\-\s]/g, '');
         tel.value = string;
-        if (string.length >= 10) {
-            if ((string.includes('(') && !string.includes(')')) || (!string.includes('(') && string.includes(')'))) {
-                signErrors['tel'] = true;
-                signBtn.setAttribute('disabled', '');
-                errorTel.innerHTML = 'Неправильно введений номер';
-            } else if (phoneRegEx1.test(string) || phoneRegEx2.test(string) || phoneRegEx3.test(string)) {
-                signErrors['tel'] = false;
-                errorTel.innerHTML = '';
-            } else {
-                if (phoneRegEx1.test(string) || phoneRegEx2.test(string) || phoneRegEx3.test(string)) {
+        if (string.length > 0) {
+            if (string.length >= 10) {
+                if ((string.includes('(') && !string.includes(')')) || (!string.includes('(') && string.includes(')'))) {
+                    signErrors['tel'] = true;
+                    disabledBtn.setAttribute('disabled', '');
+                    errorTel.innerHTML = 'Неправильно введений номер';
+                } else if (phoneRegEx1.test(string) || phoneRegEx2.test(string) || phoneRegEx3.test(string)) {
                     signErrors['tel'] = false;
                     errorTel.innerHTML = '';
                 } else {
-                    signErrors['tel'] = true;
-                    signBtn.setAttribute('disabled', '');
-                    errorTel.innerHTML = 'Неправильно введений номер';
+                    if (phoneRegEx1.test(string) || phoneRegEx2.test(string) || phoneRegEx3.test(string)) {
+                        signErrors['tel'] = false;
+                        errorTel.innerHTML = '';
+                    } else {
+                        signErrors['tel'] = true;
+                        disabledBtn.setAttribute('disabled', '');
+                        errorTel.innerHTML = 'Неправильно введений номер';
+                    }
                 }
+            } else {
+                signErrors['tel'] = true;
+                disabledBtn.setAttribute('disabled', '');
             }
         } else {
-            signErrors['tel'] = true;
-            signBtn.setAttribute('disabled', '');
+            signErrors['tel'] = false;
         }
-        // console.log(signErrors['tel'], signErrors['email']);
         if (!signErrors['tel'] && !signErrors['email']) {
-            signBtn.removeAttribute('disabled');
+            disabledBtn.removeAttribute('disabled');
         }
     }
 
     // Email check through RegEx
     const emails = document.querySelectorAll('.email'),
-        emailRegEx = /^([a-z][a-z0-9._-]+[a-z])(@[a-z][a-z0-9_-]+)(\.[a-z]+)(\.[a-z]+)?$/gi;
+        emailRegEx = /^([a-z][a-z0-9._-]+)(@[a-z][a-z0-9_-]+)(\.[a-z][a-z]+)(\.[a-z][a-z]+)?$/gi;
     signErrors['email'] = true;
     emails.forEach(function (email) {
         const errorEmail = email.nextElementSibling;
+        let disabledBtn = email.closest('form').querySelector('.disabledBtn');
         email.oninput = function () {
             let string = email.value;
             if (string.length >= 10) {
@@ -136,17 +151,16 @@ document.addEventListener('DOMContentLoaded', function () {
                         errorEmail.innerHTML = '';
                     } else {
                         signErrors['email'] = true;
-                        signBtn.setAttribute('disabled', '');
+                        disabledBtn.setAttribute('disabled', '');
                         errorEmail.innerHTML = 'Такої ел. адреси не існує';
                     }
                 }
             } else {
                 signErrors['email'] = true;
-                signBtn.setAttribute('disabled', '');
+                disabledBtn.setAttribute('disabled', '');
             }
-            // console.log(signErrors['tel'], signErrors['email']);
             if (!signErrors['tel'] && !signErrors['email']) {
-                signBtn.removeAttribute('disabled');
+                disabledBtn.removeAttribute('disabled');
             }
         }
     });
@@ -154,17 +168,10 @@ document.addEventListener('DOMContentLoaded', function () {
     // Sign popup
     const popup = document.querySelector('#sign_popup'),
         closeBtn = popup.querySelector('#closeBtn');
+        // Show popup
     function showPopup() {
         popup.classList.remove('hidden');
         document.body.classList.add('over_hidden');
-
-        // Close popup
-        function closePopup() {
-            popup.classList.add('hidden');
-            document.body.classList.remove('over_hidden');
-            signErrors['tel'] = false;
-            signErrors['email'] = false;
-        }
         popup.addEventListener('click', function (ev) {
             if (ev.target === popup) closePopup();
         });
@@ -173,7 +180,34 @@ document.addEventListener('DOMContentLoaded', function () {
         });
         closeBtn.onclick = closePopup;
     }
+        // Close popup
+    function closePopup() {
+        popup.classList.add('hidden');
+        document.body.classList.remove('over_hidden');
+        signErrors['tel'] = false;
+        signErrors['email'] = false;
+    }
     document.querySelector('#getStarted').addEventListener('click', showPopup);
+
+    // Registration imitation
+    const confirmBtn = document.querySelector('#signBtn');
+    confirmBtn.addEventListener('click', function() {
+        const name = confirmBtn.closest('form').querySelector('[name="name"]'),
+            email = confirmBtn.closest('form').querySelector('[name="email"]'),
+            tel = confirmBtn.closest('form').querySelector('[name="tel"]');
+        let userData = {
+            'name': name.value, 
+            'email': email.value, 
+            'tel': tel.value,
+        };
+        localStorage.setItem('user', JSON.stringify(userData));
+        name.value = null;
+        email.value = null;
+        tel.value = null;
+        closePopup();
+    });
+    // localStorage.clear();
+    console.log(localStorage.getItem('user'));
 
     // triple click focus
     const hackedElems = document.querySelectorAll('.hacked');
@@ -200,5 +234,63 @@ document.addEventListener('DOMContentLoaded', function () {
             clickCount = 0;
         }, 500);
     });
+
+    // Fold/Unfold posts' full text
+    const texts = document.querySelectorAll('#blog .text'),
+        readBtns = document.querySelectorAll('#blog .readMore');
+    let fullTexts = [];
+    texts.forEach(function(text, index) {
+        if (text.innerText.length > 150) {
+            fullTexts[index] = text.innerText;
+            text.innerText = text.innerText.slice(0, 145) + ' ...';
+        }
+    });
+    readBtns.forEach(function(btn, index) {
+        let isFull = false;
+        function toggleText(ev) {
+            ev.preventDefault();
+            if (!isFull) {
+                texts[index].innerText = fullTexts[index];
+                btn.innerText = 'Show less';
+                isFull = true;
+            } else {
+                texts[index].innerText = texts[index].innerText.slice(0, 145) + ' ...';
+                btn.innerText = 'Read more';
+                isFull = false;
+            }
+        }
+        btn.addEventListener('click', function(ev) { toggleText(ev) });
+    });
+
+    // Confirm sending form
+    const form = document.querySelector('#contactForm'),
+        confirmPopup = document.querySelector('#sendMsgPopup'),
+        showConfirmBtn = form.querySelector('#showConfirmBtn'),
+        cancelBtn = form.querySelector('#cancelBtn');
+    function showConfirmPopup() {
+        confirmPopup.classList.remove('hidden');
+
+        // Close confirmPopup
+        function closePopup() {
+            confirmPopup.classList.add('hidden');
+            signErrors['email'] = false;
+        }
+        confirmPopup.addEventListener('click', function (ev) {
+            if (ev.target === confirmPopup) closePopup();
+        });
+        document.addEventListener("keydown", function (ev) {
+            if (ev.key === "Escape") closePopup();
+        });
+        cancelBtn.onclick = closePopup;
+    }
+    showConfirmBtn.addEventListener('click', showConfirmPopup);
+
+    // Fill registered user's data into message form
+    if (localStorage.getItem('user') !== null) {
+        let user = JSON.parse(localStorage.getItem('user'));
+        form.querySelector('[name="name"]').value = user.name;
+        form.querySelector('[name="email"]').value = user.email;
+        showConfirmBtn.removeAttribute('disabled');
+    }
 
 });
